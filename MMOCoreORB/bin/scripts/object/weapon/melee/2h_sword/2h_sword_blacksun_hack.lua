9316faec41698a2b75fd78040997ede510ff54d4
Levarrishawk <levarrishawk@gmail.com>
2016-05-31 00:22:06 -0400

    [test] 2h-Sword Script Manual Insertion

--- MMOCoreORB/bin/scripts/object/weapon/melee/2h_sword/2h_sword_blacksun_hack.lua
+++ MMOCoreORB/bin/scripts/object/weapon/melee/2h_sword/2h_sword_blacksun_hack.lua
@@ -43,93 +43,93 @@
 
 object_weapon_melee_2h_sword_2h_sword_blacksun_hack = object_weapon_melee_2h_sword_shared_2h_sword_blacksun_hack:new {
 
-	playerRaces = { "object/creature/player/bothan_male.iff",
-				"object/creature/player/bothan_female.iff",
-				"object/creature/player/human_male.iff",
-				"object/creature/player/human_female.iff",
-				"object/creature/player/ithorian_male.iff",
-				"object/creature/player/ithorian_female.iff",
-				"object/creature/player/moncal_male.iff",
-				"object/creature/player/moncal_female.iff",
-				"object/creature/player/rodian_male.iff",
-				"object/creature/player/rodian_female.iff",
-				"object/creature/player/sullustan_male.iff",
-				"object/creature/player/sullustan_female.iff",
-				"object/creature/player/trandoshan_male.iff",
-				"object/creature/player/trandoshan_female.iff",
-				"object/creature/player/twilek_male.iff",
-				"object/creature/player/twilek_female.iff",
-				"object/creature/player/wookiee_male.iff",
-				"object/creature/player/wookiee_female.iff",
-				"object/creature/player/zabrak_male.iff",
-				"object/creature/player/zabrak_female.iff" },
-	
-	-- RANGEDATTACK, MELEEATTACK, FORCEATTACK, TRAPATTACK, GRENADEATTACK, HEAVYACIDBEAMATTACK,
-	-- HEAVYLIGHTNINGBEAMATTACK, HEAVYPARTICLEBEAMATTACK, HEAVYROCKETLAUNCHERATTACK, HEAVYLAUNCHERATTACK
-	attackType = MELEEATTACK,
-	
-	-- ENERGY, KINETIC, ELECTRICITY, STUN, BLAST, HEAT, COLD, ACID, LIGHTSABER
-	damageType = KINETIC,
-	
-	-- NONE, LIGHT, MEDIUM, HEAVY
-	armorPiercing = MEDIUM,
-	
-	-- combat_rangedspecialize_bactarifle, combat_rangedspecialize_rifle, combat_rangedspecialize_pistol, combat_rangedspecialize_heavy, combat_rangedspecialize_carbine
-	-- combat_meleespecialize_unarmed, combat_meleespecialize_twohand, combat_meleespecialize_polearm, combat_meleespecialize_onehand, combat_general,
-	-- combat_meleespecialize_twohandlightsaber, combat_meleespecialize_polearmlightsaber, combat_meleespecialize_onehandlightsaber
-	xpType = "combat_meleespecialize_twohand",
-	       
-	-- See http://www.ocdsoft.com/files/certifications.xls
-	certificationsRequired = { "combat_2hsword_speed_02" },
-	-- See http://www.ocdsoft.com/files/accuracy.xls
-	creatureAccuracyModifiers = { "twohandmelee_accuracy" },
-	
-	-- See http://www.ocdsoft.com/files/defense.xls
-	defenderDefenseModifiers = { "melee_defense" },
-	
-	-- Leave as "dodge" for now, may have additions later
-	defenderSecondaryDefenseModifiers = { "counterattack" },
+  playerRaces = { "object/creature/player/bothan_male.iff",
+        "object/creature/player/bothan_female.iff",
+        "object/creature/player/human_male.iff",
+        "object/creature/player/human_female.iff",
+        "object/creature/player/ithorian_male.iff",
+        "object/creature/player/ithorian_female.iff",
+        "object/creature/player/moncal_male.iff",
+        "object/creature/player/moncal_female.iff",
+        "object/creature/player/rodian_male.iff",
+        "object/creature/player/rodian_female.iff",
+        "object/creature/player/sullustan_male.iff",
+        "object/creature/player/sullustan_female.iff",
+        "object/creature/player/trandoshan_male.iff",
+        "object/creature/player/trandoshan_female.iff",
+        "object/creature/player/twilek_male.iff",
+        "object/creature/player/twilek_female.iff",
+        "object/creature/player/wookiee_male.iff",
+        "object/creature/player/wookiee_female.iff",
+        "object/creature/player/zabrak_male.iff",
+        "object/creature/player/zabrak_female.iff" },
+  
+  -- RANGEDATTACK, MELEEATTACK, FORCEATTACK, TRAPATTACK, GRENADEATTACK, HEAVYACIDBEAMATTACK,
+  -- HEAVYLIGHTNINGBEAMATTACK, HEAVYPARTICLEBEAMATTACK, HEAVYROCKETLAUNCHERATTACK, HEAVYLAUNCHERATTACK
+  attackType = MELEEATTACK,
+  
+  -- ENERGY, KINETIC, ELECTRICITY, STUN, BLAST, HEAT, COLD, ACID, FORCE, LIGHTSABER
+  damageType = KINETIC,
+  
+  -- NONE, LIGHT, MEDIUM, HEAVY
+  armorPiercing = MEDIUM,
+  
+  -- combat_rangedspecialize_bactarifle, combat_rangedspecialize_rifle, combat_rangedspecialize_pistol, combat_rangedspecialize_heavy, combat_rangedspecialize_carbine
+  -- combat_meleespecialize_unarmed, combat_meleespecialize_twohand, combat_meleespecialize_polearm, combat_meleespecialize_onehand, combat_general,
+  -- combat_meleespecialize_twohandlightsaber, combat_meleespecialize_polearmlightsaber, combat_meleespecialize_onehandlightsaber
+  xpType = "combat_meleespecialize_twohand",
+         
+  -- See http://www.ocdsoft.com/files/certifications.xls
+  certificationsRequired = { "combat_2hsword_speed_02" },
+  -- See http://www.ocdsoft.com/files/accuracy.xls
+  creatureAccuracyModifiers = { "twohandmelee_accuracy" },
+  
+  -- See http://www.ocdsoft.com/files/defense.xls
+  defenderDefenseModifiers = { "melee_defense" },
+  
+  -- Leave as "dodge" for now, may have additions later
+  defenderSecondaryDefenseModifiers = { "counterattack" },
 
-	defenderToughnessModifiers = { "twohandmelee_toughness" },
-	
-	-- See http://www.ocdsoft.com/files/speed.xls
-	speedModifiers = { "twohandmelee_speed" },
-	
-	-- Leave blank for now
-	damageModifiers = { },
-	       
-	
-	-- The values below are the default values.  To be used for blue frog objects primarily
-	healthAttackCost = 49,
-	actionAttackCost = 74,
-	mindAttackCost = 42,
-	forceCost = 0,
-	
-	pointBlankAccuracy = -3,
-	pointBlankRange = 0,
-	
-	idealRange = 3,
-	idealAccuracy = -9,
-	
-	maxRange = 5,
-	maxRangeAccuracy = 0,
+  defenderToughnessModifiers = { "twohandmelee_toughness" },
+  
+  -- See http://www.ocdsoft.com/files/speed.xls
+  speedModifiers = { "twohandmelee_speed" },
+  
+  -- Leave blank for now
+  damageModifiers = { },
+         
+  
+  -- The values below are the default values.  To be used for blue frog objects primarily
+  healthAttackCost = 0,
+  actionAttackCost = 300,
+  mindAttackCost = 0,
+  forceCost = 0,
+  
+  pointBlankAccuracy = -3,
+  pointBlankRange = 0,
+  
+  idealRange = 3,
+  idealAccuracy = -9,
+  
+  maxRange = 5,
+  maxRangeAccuracy = 0,
 
-	minDamage = 171,
-        maxDamage = 300,
-	
-	attackSpeed = 2.7,
+  minDamage = 171,
+        maxDamage = 250,
+  
+  attackSpeed = 1,
 
-	woundsRatio = 18,
+  woundsRatio = 18,
 
-	numberExperimentalProperties = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	experimentalProperties = {"XX", "XX", "SR", "SR", "SR", "SR", "SR", "SR", "SR", "XX", "SR", "XX", "SR", "SR", "SR"},
-	experimentalWeights = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	experimentalGroupTitles = {"null", "null", "expDamage", "expDamage", "expDamage", "expDamage", "exp_durability", "expRange", "expRange", "null", "expRange", "null", "expEffeciency", "expEffeciency", "expEffeciency"},
-	experimentalSubGroupTitles = {"null", "null", "mindamage", "maxdamage", "attackspeed", "woundchance", "hitpoints", "zerorangemod", "maxrangemod", "midrange", "midrangemod", "maxrange", "attackhealthcost", "attackactioncost", "attackmindcost"},
-	experimentalMin = {0, 0, 58, 178, 5.2, 12, 750, -22, -18, 3, -28, 6, 78, 48, 12},
-	experimentalMax = {0, 0, 112, 246, 3.8, 24, 1500, 8, 12, 3, 2, 6, 46, 32, 4},
-	experimentalPrecision = {0, 0, 0, 0, 1, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0},
-	experimentalCombineType = {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+  numberExperimentalProperties = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+  experimentalProperties = {"XX", "XX", "SR", "SR", "SR", "SR", "SR", "SR", "SR", "XX", "SR", "XX", "XX", "XX", "XX"},
+  experimentalWeights = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+  experimentalGroupTitles = {"null", "null", "expDamage", "expDamage", "expDamage", "expDamage", "exp_durability", "expRange", "expRange", "null", "expRange", "null", "null", "null", "null"},
+  experimentalSubGroupTitles = {"null", "null", "mindamage", "maxdamage", "attackspeed", "woundchance", "hitpoints", "zerorangemod", "maxrangemod", "midrange", "midrangemod", "maxrange", "attackhealthcost", "attackactioncost", "attackmindcost"},
+  experimentalMin = {0, 0, 58, 178, 1, 12, 750, -22, -18, 3, -28, 6, 0, 300, 0},
+  experimentalMax = {0, 0, 112, 246, 1, 24, 1500, 8, 12, 3, 2, 6, 0, 300, 0},
+  experimentalPrecision = {0, 0, 0, 0, 1, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0},
+  experimentalCombineType = {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 }
 
 ObjectTemplates:addTemplate(object_weapon_melee_2h_sword_2h_sword_blacksun_hack, "object/weapon/melee/2h_sword/2h_sword_blacksun_hack.iff")
--- MMOCoreORB/bin/scripts/object/weapon/melee/2h_sword/2h_sword_maul.lua
+++ MMOCoreORB/bin/scripts/object/weapon/melee/2h_sword/2h_sword_maul.lua
@@ -43,93 +43,93 @@
 
 object_weapon_melee_2h_sword_2h_sword_maul = object_weapon_melee_2h_sword_shared_2h_sword_maul:new {
 
-	playerRaces = { "object/creature/player/bothan_male.iff",
-				"object/creature/player/bothan_female.iff",
-				"object/creature/player/human_male.iff",
-				"object/creature/player/human_female.iff",
-				"object/creature/player/ithorian_male.iff",
-				"object/creature/player/ithorian_female.iff",
-				"object/creature/player/moncal_male.iff",
-				"object/creature/player/moncal_female.iff",
-				"object/creature/player/rodian_male.iff",
-				"object/creature/player/rodian_female.iff",
-				"object/creature/player/sullustan_male.iff",
-				"object/creature/player/sullustan_female.iff",
-				"object/creature/player/trandoshan_male.iff",
-				"object/creature/player/trandoshan_female.iff",
-				"object/creature/player/twilek_male.iff",
-				"object/creature/player/twilek_female.iff",
-				"object/creature/player/wookiee_male.iff",
-				"object/creature/player/wookiee_female.iff",
-				"object/creature/player/zabrak_male.iff",
-				"object/creature/player/zabrak_female.iff" },
+  playerRaces = { "object/creature/player/bothan_male.iff",
+        "object/creature/player/bothan_female.iff",
+        "object/creature/player/human_male.iff",
+        "object/creature/player/human_female.iff",
+        "object/creature/player/ithorian_male.iff",
+        "object/creature/player/ithorian_female.iff",
+        "object/creature/player/moncal_male.iff",
+        "object/creature/player/moncal_female.iff",
+        "object/creature/player/rodian_male.iff",
+        "object/creature/player/rodian_female.iff",
+        "object/creature/player/sullustan_male.iff",
+        "object/creature/player/sullustan_female.iff",
+        "object/creature/player/trandoshan_male.iff",
+        "object/creature/player/trandoshan_female.iff",
+        "object/creature/player/twilek_male.iff",
+        "object/creature/player/twilek_female.iff",
+        "object/creature/player/wookiee_male.iff",
+        "object/creature/player/wookiee_female.iff",
+        "object/creature/player/zabrak_male.iff",
+        "object/creature/player/zabrak_female.iff" },
 
-	-- RANGEDATTACK, MELEEATTACK, FORCEATTACK, TRAPATTACK, GRENADEATTACK, HEAVYACIDBEAMATTACK,
-	-- HEAVYLIGHTNINGBEAMATTACK, HEAVYPARTICLEBEAMATTACK, HEAVYROCKETLAUNCHERATTACK, HEAVYLAUNCHERATTACK
-	attackType = MELEEATTACK,
+  -- RANGEDATTACK, MELEEATTACK, FORCEATTACK, TRAPATTACK, GRENADEATTACK, HEAVYACIDBEAMATTACK,
+  -- HEAVYLIGHTNINGBEAMATTACK, HEAVYPARTICLEBEAMATTACK, HEAVYROCKETLAUNCHERATTACK, HEAVYLAUNCHERATTACK
+  attackType = MELEEATTACK,
 
-	-- ENERGY, KINETIC, ELECTRICITY, STUN, BLAST, HEAT, COLD, ACID, LIGHTSABER
-	damageType = BLAST,
+  -- ENERGY, KINETIC, ELECTRICITY, STUN, BLAST, HEAT, COLD, ACID, FORCE, LIGHTSABER
+  damageType = BLAST,
 
-	-- NONE, LIGHT, MEDIUM, HEAVY
-	armorPiercing = MEDIUM,
+  -- NONE, LIGHT, MEDIUM, HEAVY
+  armorPiercing = HEAVY,
 
-	-- combat_rangedspecialize_bactarifle, combat_rangedspecialize_rifle, combat_rangedspecialize_pistol, combat_rangedspecialize_heavy, combat_rangedspecialize_carbine
-	-- combat_meleespecialize_unarmed, combat_meleespecialize_twohand, combat_meleespecialize_polearm, combat_meleespecialize_onehand, combat_general,
-	-- combat_meleespecialize_twohandlightsaber, combat_meleespecialize_polearmlightsaber, combat_meleespecialize_onehandlightsaber
-	xpType = "combat_meleespecialize_twohand",
+  -- combat_rangedspecialize_bactarifle, combat_rangedspecialize_rifle, combat_rangedspecialize_pistol, combat_rangedspecialize_heavy, combat_rangedspecialize_carbine
+  -- combat_meleespecialize_unarmed, combat_meleespecialize_twohand, combat_meleespecialize_polearm, combat_meleespecialize_onehand, combat_general,
+  -- combat_meleespecialize_twohandlightsaber, combat_meleespecialize_polearmlightsaber, combat_meleespecialize_onehandlightsaber
+  xpType = "combat_meleespecialize_twohand",
 
-	-- See http://www.ocdsoft.com/files/certifications.xls
-	certificationsRequired = { "cert_sword_2h_maul" },
-	-- See http://www.ocdsoft.com/files/accuracy.xls
-	creatureAccuracyModifiers = { "twohandmelee_accuracy" },
+  -- See http://www.ocdsoft.com/files/certifications.xls
+  certificationsRequired = { "cert_sword_2h_maul" },
+  -- See http://www.ocdsoft.com/files/accuracy.xls
+  creatureAccuracyModifiers = { "twohandmelee_accuracy" },
 
-	-- See http://www.ocdsoft.com/files/defense.xls
-	defenderDefenseModifiers = { "melee_defense" },
+  -- See http://www.ocdsoft.com/files/defense.xls
+  defenderDefenseModifiers = { "melee_defense" },
 
-	-- Leave as "dodge" for now, may have additions later
-	defenderSecondaryDefenseModifiers = { "counterattack" },
+  -- Leave as "dodge" for now, may have additions later
+  defenderSecondaryDefenseModifiers = { "counterattack" },
 
-	defenderToughnessModifiers = { "twohandmelee_toughness" },
+  defenderToughnessModifiers = { "twohandmelee_toughness" },
 
-	-- See http://www.ocdsoft.com/files/speed.xls
-	speedModifiers = { "twohandmelee_speed" },
+  -- See http://www.ocdsoft.com/files/speed.xls
+  speedModifiers = { "twohandmelee_speed" },
 
-	-- Leave blank for now
-	damageModifiers = { },
+  -- Leave blank for now
+  damageModifiers = { "swordsman" },
 
 
-	-- The values below are the default values.  To be used for blue frog objects primarily
-	healthAttackCost = 95,
-	actionAttackCost = 25,
-	mindAttackCost = 10,
-	forceCost = 0,
+  -- The values below are the default values.  To be used for blue frog objects primarily
+  healthAttackCost = 0,
+  actionAttackCost = 300,
+  mindAttackCost = 0,
+  forceCost = 0,
 
-	pointBlankRange = 0,
-	pointBlankAccuracy = -35,
+  pointBlankRange = 0,
+  pointBlankAccuracy = 0,
 
-	idealRange = 3,
-	idealAccuracy = -35,
+  idealRange = 3,
+  idealAccuracy = 0,
 
-	maxRange = 5,
-	maxRangeAccuracy = -35,
+  maxRange = 5,
+  maxRangeAccuracy = 0,
 
-	minDamage = 100,
-	maxDamage = 450,
+  minDamage = 400,
+  maxDamage = 500,
 
-	attackSpeed = 6,
+  attackSpeed = 1,
 
-	woundsRatio = 15,
+  woundsRatio = 15,
 
-	numberExperimentalProperties = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	experimentalProperties = {"XX", "XX", "SR", "SR", "SR", "SR", "SR", "SR", "SR", "XX", "SR", "XX", "SR", "SR", "SR"},
-	experimentalWeights = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	experimentalGroupTitles = {"null", "null", "expDamage", "expDamage", "expDamage", "expDamage", "exp_durability", "expRange", "expRange", "null", "expRange", "null", "expEffeciency", "expEffeciency", "expEffeciency"},
-	experimentalSubGroupTitles = {"null", "null", "mindamage", "maxdamage", "attackspeed", "woundchance", "hitpoints", "zerorangemod", "maxrangemod", "midrange", "midrangemod", "maxrange", "attackhealthcost", "attackactioncost", "attackmindcost"},
-	experimentalMin = {0, 0, 53, 235, 8.5, 11, 750, -46, -46, 3, -46, 4, 120, 30, 16},
-	experimentalMax = {0, 0, 98, 436, 5.8, 20, 1500, -25, -25, 3, -25, 4, 63, 15, 7},
-	experimentalPrecision = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	experimentalCombineType = {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+  numberExperimentalProperties = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+  experimentalProperties = {"XX", "XX", "SR", "SR", "SR", "SR", "SR", "SR", "SR", "XX", "SR", "XX", "XX", "XX", "XX"},
+  experimentalWeights = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+  experimentalGroupTitles = {"null", "null", "expDamage", "expDamage", "expDamage", "expDamage", "exp_durability", "expRange", "expRange", "null", "expRange", "null", "null", "null", "null"},
+  experimentalSubGroupTitles = {"null", "null", "mindamage", "maxdamage", "attackspeed", "woundchance", "hitpoints", "zerorangemod", "maxrangemod", "midrange", "midrangemod", "maxrange", "attackhealthcost", "attackactioncost", "attackmindcost"},
+  experimentalMin = {0, 0, 53, 235, 1, 11, 750, -46, -46, 3, -46, 4, 0, 300, 0},
+  experimentalMax = {0, 0, 98, 436, 1, 20, 1500, -25, -25, 3, -25, 4, 0, 300, 0},
+  experimentalPrecision = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  experimentalCombineType = {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 }
 
 ObjectTemplates:addTemplate(object_weapon_melee_2h_sword_2h_sword_maul, "object/weapon/melee/2h_sword/2h_sword_maul.iff")
