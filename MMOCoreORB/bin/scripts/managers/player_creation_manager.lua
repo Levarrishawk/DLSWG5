ca672530954fb8738516c831f364ff952497c2f5
Levarrishawk <levarrishawk@gmail.com>
2016-05-31 04:43:35 -0400

    CharacterCreation Recovery

--- MMOCoreORB/bin/scripts/managers/player_creation_manager.lua
+++ MMOCoreORB/bin/scripts/managers/player_creation_manager.lua
@@ -9,7 +9,7 @@ startingBank = 1000
 --How many skill points a new characters start with
 skillPoints = 250
 
-professions = {
+--[[professions = {
 	"combat_brawler",
 	"combat_marksman",
 	"crafting_artisan",
@@ -17,6 +17,21 @@ professions = {
 	"outdoors_scout",
 	"science_medic",
 	"social_entertainer"
+}--]]
+
+professions = {
+  "combat_jedi",
+  "combat_officer",
+  "combat_mando",
+  "combat_assassin",
+  "combat_medic",
+  "combat_soldier",
+  "combat_shocktrooper",
+  "trader_structures",
+  "trader_engineering",
+  "trader_munitions",
+  "trader_pharmacist",
+  "trader_domestics"
 }
 
 marksmanPistol = "object/weapon/ranged/pistol/pistol_cdef.iff"
@@ -47,14 +62,31 @@ marojMelon = "object/tangible/food/foraged/foraged_fruit_s1.iff"
 
 x31Speeder = "object/tangible/deed/vehicle_deed/landspeeder_x31_deed.iff"
 
-professionSpecificItems = {
-	combat_brawler = { brawlerOneHander, brawlerTwoHander, brawlerPolearm },
-	combat_marksman = { marksmanPistol, marksmanCarbine, marksmanRifle },
-	crafting_artisan = { genericTool, mineralTool, chemicalTool },
-	jedi = { },
-	outdoors_scout = { genericTool },
-	science_medic = { foodTool },
-	social_entertainer = { slitherhorn }
+robe = "object/tangible/wearables/robe/robe_jedi_padawan.iff"
+
+saber = "object/weapon/melee/sword/crafted_saber/sword_lightsaber_s9_training.iff"
+
+jediTool = "object/tangible/crafting/station/jedi_tool.iff"
+
+jediCrystal = "object/tangible/component/weapon/lightsaber/lightsaber_module_force_crystal.iff"
+
+gasTool = "object/tangible/survey_tool/survey_tool_gas.iff"
+
+
+professionSpecificItems = { 
+  combat_soldier = { marksmanPistol, marksmanCarbine, marksmanRifle },
+  combat_mando = { marksmanPistol, marksmanCarbine, marksmanRifle },
+  combat_assassin = { marksmanPistol, marksmanCarbine, marksmanRifle },
+  combat_shocktrooper = { marksmanPistol, marksmanCarbine, marksmanRifle },
+  combat_jedi = { genericTool, mineralTool, chemicalTool, gasTool, jediTool, jediCrystal },
+  combat_officer = { genericTool, marksmanPistol, marksmanCarbine, marksmanRifle  },
+  combat_medic = { foodTool, marksmanPistol, brawlerPolearm },
+  trader_structures = { genericTool, mineralTool, chemicalTool, gasTool },
+  trader_munitions = { genericTool, mineralTool, chemicalTool, gasTool}, 
+  trader_engineering = { genericTool, mineralTool, chemicalTool, gasTool }, 
+  trader_domestics = { genericTool, mineralTool, chemicalTool, foodTool, gasTool },
+  trader_pharmacist = { genericTool, mineralTool, chemicalTool, foodTool, gasTool },     
+--  social_entertainer = { slitherhorn }
 }
 
 commonStartingItems = { marojMelon, survivalKnife, x31Speeder }
