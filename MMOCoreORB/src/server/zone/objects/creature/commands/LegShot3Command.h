33184e886dfee21223e98be14ad2a9ef44a29add
Levarrishawk <levarrishawk@gmail.com>
2016-07-01 20:39:52 -0400

    [iteration] Light infantry snare effect

--- MMOCoreORB/src/server/zone/objects/creature/commands/LegShot3Command.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/LegShot3Command.h
@@ -92,7 +92,7 @@ public:
 							//Send messages, and effect
 							creature->sendSystemMessage("You snare your target!");
 							creatureTarget->sendSystemMessage("You've been snared!");
-							creatureTarget->playEffect("clienteffect/pl_force_resist_bleeding_self.cef", "");
+							creatureTarget->playEffect("clienteffect/space_command/scram_reactor_sparking.cef", "");
 
 						}
 
