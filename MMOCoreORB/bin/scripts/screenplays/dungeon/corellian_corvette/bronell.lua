4638eeec1a0216118699c67268bafccc4749a2ca
Levarrishawk <levarrishawk@gmail.com>
2016-07-02 00:37:43 -0400

    [re-implentation] Neutral Vette -> Chandrila

--- MMOCoreORB/bin/scripts/screenplays/dungeon/corellian_corvette/bronell.lua
+++ MMOCoreORB/bin/scripts/screenplays/dungeon/corellian_corvette/bronell.lua
@@ -1,33 +1,33 @@
 
-bronellIntel =	{
-			containerIds = { 3795397, 3915469, 4105781 }, -- Talus, Naboo, Lok
-			itemTemplates = { "object/tangible/loot/dungeon/corellian_corvette/neutral_assassin_filler01.iff", "object/tangible/loot/dungeon/corellian_corvette/neutral_assassin_filler02.iff", "object/tangible/loot/dungeon/corellian_corvette/neutral_assassin_intel.iff" },
-		}
+bronellIntel =  {
+      containerIds = { 3795397, 3915469, 4105781 }, -- Talus, Naboo, Lok
+      itemTemplates = { "object/tangible/loot/dungeon/corellian_corvette/neutral_assassin_filler01.iff", "object/tangible/loot/dungeon/corellian_corvette/neutral_assassin_filler02.iff", "object/tangible/loot/dungeon/corellian_corvette/neutral_assassin_intel.iff" },
+    }
 
-bronellTicketInfo = { depPlanet = "tatooine", faction = "neutral", missionType = "assassination" }
+bronellTicketInfo = { depPlanet = "chandrila", faction = "neutral", missionType = "assassination" }
 
-bronellCompensation = { { compType = "credits", amount = 500 } }
+bronellCompensation = { { compType = "credits", amount = 15000 } }
 
-bronell = { planetName = "tatooine", npcTemplate = "bronell", x = -6.7, z = 5.8, y = 97.9, direction = 180, cellID = 1177469, position = STAND }
+bronell = { planetName = "chandrila", npcTemplate = "bronell", x = 31.7, z = 0.1, y = 4.7, direction = 172, cellID = 35791443, position = STAND }
 
 ticketGiverBronell = CorvetteTicketGiverLogic:new {
-	numberOfActs = 1,
-	npc = bronell,
-	intelMap = bronellIntel,
-	ticketInfo = bronellTicketInfo,
-	giverName = "ticketGiverBronell",
-	faction = 0,
-	compensation = bronellCompensation,
-	badgeNumber = 117,
-	menuComponent = "BronellIntelSearchMenuComponent",
+  numberOfActs = 1,
+  npc = bronell,
+  intelMap = bronellIntel,
+  ticketInfo = bronellTicketInfo,
+  giverName = "ticketGiverBronell",
+  faction = 0,
+  compensation = bronellCompensation,
+  badgeNumber = 117,
+  menuComponent = "BronellIntelSearchMenuComponent",
 }
 
 registerScreenPlay("ticketGiverBronell", true)
 
 ticketGiverBronellConvoHandler = CorvetteTicketGiverConvoHandler:new {
-	ticketGiver = ticketGiverBronell
+  ticketGiver = ticketGiverBronell
 }
 
 BronellIntelSearchMenuComponent = IntelSearchMenuComponent:new {
-	ticketGiver = ticketGiverBronell
+  ticketGiver = ticketGiverBronell
 }
