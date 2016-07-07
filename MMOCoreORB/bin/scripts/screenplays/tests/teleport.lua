1cb4def64cd5fa2698f0d1147055579393fbb74f
Skyyyr <skydeath227@yahoo.com>
2016-07-06 23:03:23 -0700

    Create teleport.lua

---
+++ MMOCoreORB/bin/scripts/screenplays/tests/teleport.lua
@@ -0,0 +1,33 @@
+--[[
+    This screenplay is to display and comment on how to have teleporting in your screenplay.
+  ]]--
+
+
+teleportTest = ScreenPlay:new { 
+	numberOfActs = 1,
+}
+
+registerScreenPlay("teleportTest", true)
+
+
+--Some of these calls may be old, and will need updated. Basically will still be the same.
+
+function force_sensitive:mellichae_damage(creatureObject, playerObject, damage)
+
+  --Basics
+	if creatureObject == nil or playerObject == nil then
+		return 0
+	end
+	
+	--Include basics
+	local player = LuaCreatureObject(playerObject)
+	local creature = LuaCreatureObject(creatureObject)
+	
+	--Test variable
+	local statementVariable = screenplayState() or conversationScreen()
+	
+	if (statementVariable == true) then
+			local creo = LuaSceneObject(creatureObject)
+			creo:switchZone("planet", 10, 0, -10, 0) -- x, z, y, cell
+	end	
+end
