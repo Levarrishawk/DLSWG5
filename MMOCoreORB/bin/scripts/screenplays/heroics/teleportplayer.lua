a0f02528c9694c8eacc5422694e7e6ca966200c9
Levarrishawk <levarrishawk@gmail.com>
2016-07-10 22:31:42 -0400

    [fixed] wrong template

--- MMOCoreORB/bin/scripts/screenplays/heroics/teleportplayer.lua
+++ MMOCoreORB/bin/scripts/screenplays/heroics/teleportplayer.lua
@@ -15,7 +15,7 @@ end
 function rebel_chasm_teleporter:spawnMobiles()
 
   --In The Guild Hall
-  spawnMobile("jakku", "rebel_chasm_dungeon_teleporter", 1, -5705, 52, 5989, 0, 0)
+  spawnMobile("jakku", "reb_chasm_dungeon_teleporter", 1, -5705, 52, 5989, 0, 0)
   end
 
 
