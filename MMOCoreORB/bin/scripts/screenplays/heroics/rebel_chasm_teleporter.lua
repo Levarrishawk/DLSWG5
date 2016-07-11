ee6f2d282c032d0320eca641e5cec64ebe10ab52
Skyyyr <skydeath227@yahoo.com>
2016-07-11 14:10:20 -0700

    Update rebel_chasm_teleporter.lua
    Added creatureObject to the function call, and changed the variable player call to reference creatureObject.

--- MMOCoreORB/bin/scripts/screenplays/heroics/rebel_chasm_teleporter.lua
+++ MMOCoreORB/bin/scripts/screenplays/heroics/rebel_chasm_teleporter.lua
@@ -46,14 +46,14 @@ function rebel_chasm_teleporter_convo_handler:getNextConversationScreen(conversa
 	return nextConversationScreen	
 end
 
-function rebel_chasm_teleporter_convo_handler:runScreenHandlers(conversationTemplate, conversingPlayer, conversingNPC, selectedOption, conversationScreen)	
+function rebel_chasm_teleporter_convo_handler:runScreenHandlers(conversationTemplate, conversingPlayer, conversingNPC, selectedOption, conversationScreen, creatureObject)	
 	--print("\ntest_convo_handler:runScreenHandlers() called\n")
 	local screen = LuaConversationScreen(conversationScreen)	
 	local screenID = screen:getScreenID()	
 	--print("screenID is " .. screenID	)
 	--local player = LuaCreatureObject(conversingPlayer)
 	
-	local player = LuaSceneObject(conversingPlayer)--This should work, if not we'd have to look at the core functions for LUA handlers.
+	local player = LuaSceneObject(creatureObject)--This should work, if not we'd have to look at the core functions for LUA handlers.
 
 	if ( screenID == "mayor2" ) then
 		 player:switchZone("dungeon2", -5.7, 12.1, -5.2, 410000010) -- x, z, y, cell
