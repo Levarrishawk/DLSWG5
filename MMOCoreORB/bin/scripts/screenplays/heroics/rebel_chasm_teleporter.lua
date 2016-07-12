af9b86f5b9829ac244c249f6a330ac0acab44591
Skyyyr <skydeath227@yahoo.com>
2016-07-11 23:48:46 -0700

    Update rebel_chasm_teleporter.lua
    Clean up, new function, and verifies.

--- MMOCoreORB/bin/scripts/screenplays/heroics/rebel_chasm_teleporter.lua
+++ MMOCoreORB/bin/scripts/screenplays/heroics/rebel_chasm_teleporter.lua
@@ -16,6 +16,16 @@ function rebel_chasm_teleporter:start()
    spawnMobile("jakku", "reb_chasm_dungeon_teleporter", 1, -5705, 52, 5989, 0, 0)
 end
 
+--Teleport function
+function rebel_chasm_teleporter:teleportPlayer(creatureObject)
+	local creo = LuaSceneObject(creatureObject)
+	if (readData("rebel_chasm_teleporter:teleportPlayer") == 1 ) then
+		writeData("rebel_chasm_teleporter:teleportPlayer",0) --resets the verify, can be removed later
+		creo:switchZone("dungeon2", -5.7, 12.1, -5.2, 410000010) -- x, z, y, cell
+	end
+end
+
+
 --The actual conversation handler
 rebel_chasm_teleporter_convo_handler = Object:new {
 	
@@ -46,17 +56,13 @@ function rebel_chasm_teleporter_convo_handler:getNextConversationScreen(conversa
 	return nextConversationScreen	
 end
 
-function rebel_chasm_teleporter_convo_handler:runScreenHandlers(conversationTemplate, conversingPlayer, conversingNPC, selectedOption, conversationScreen, creatureObject)	
-	--print("\ntest_convo_handler:runScreenHandlers() called\n")
+function rebel_chasm_teleporter_convo_handler:runScreenHandlers(conversationTemplate, conversingPlayer, conversingNPC, selectedOption, conversationScreen)
 	local screen = LuaConversationScreen(conversationScreen)	
 	local screenID = screen:getScreenID()	
-	--print("screenID is " .. screenID	)
-	--local player = LuaCreatureObject(conversingPlayer)
-	
-	local player = LuaSceneObject(creatureObject)--This should work, if not we'd have to look at the core functions for LUA handlers.
 
 	if ( screenID == "mayor2" ) then
-		 player:switchZone("dungeon2", -5.7, 12.1, -5.2, 410000010) -- x, z, y, cell
+		writeData("rebel_chasm_teleporter:teleportPlayer", 1) --Verifying step, can be removed later.
+		createEvent(500, "rebel_chasm_teleporter", "teleportPlayer", conversingPlayer) --Activates the function
 	end
 	
 	
