4bece5d509ef29897123bcb2cf815b99af782c9e
TheSynge <john0684@hotmail.com>
2016-07-12 20:16:44 -0600

    initial area spawns

--- MMOCoreORB/bin/scripts/screenplays/heroics/tps/the_power_station.lua
+++ MMOCoreORB/bin/scripts/screenplays/heroics/tps/the_power_station.lua
@@ -13,5 +13,14 @@ end
 
 function ThePowerStationScreenPlay:spawnMobiles()
 	spawnMobile("dungeon2", "tps_battle_droid", 300, -21.8, -7.0, -12.1, 97, 410000011)
+	spawnMobile("dungeon2", "tps_battle_droid", 300, -21.8, -7.0, -19.1, 97, 410000011)
+	spawnMobile("dungeon2", "tps_battle_droid", 300, -21.8, -7.0, -12.1, 97, 410000011)
+	spawnMobile("dungeon2", "tps_battle_droid", 300, -21.8, -7.0, -28.1, 97, 410000011)
+	spawnMobile("dungeon2", "tps_battle_droid", 300, -21.8, -7.0, -33.6, 97, 410000011)
+	spawnMobile("dungeon2", "tps_battle_droid", 300, -21.8, -7.0, -5.1, 97, 410000012)
+	spawnMobile("dungeon2", "tps_battle_droid", 300, -21.8, -7.0, -1.1, 97, 410000012)
+	spawnMobile("dungeon2", "tps_battle_droid", 300, -16, -7.0, -0, 97, 410000012)
+	spawnMobile("dungeon2", "tps_battle_droid", 300, -15.9, -7.0, -33.2, 97, 410000021)
+	spawnMobile("dungeon2", "tps_battle_droid", 300, -28.4, -7.0, -32.8, 90, 410000021)
 
 end
