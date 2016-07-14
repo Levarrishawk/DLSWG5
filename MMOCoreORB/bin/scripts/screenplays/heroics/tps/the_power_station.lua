09a49b34fea9482c5e0ede272eaf398247797be9
TheSynge <john0684@hotmail.com>
2016-07-13 21:01:29 -0600

    spawning TPS NPC's

--- MMOCoreORB/bin/scripts/screenplays/heroics/tps/the_power_station.lua
+++ MMOCoreORB/bin/scripts/screenplays/heroics/tps/the_power_station.lua
@@ -13,6 +13,6 @@ end
 
 function ThePowerStationScreenPlay:spawnMobiles()
 	spawnMobile("dungeon2", "tps_battle_droid", 300, -21.8, -7.0, -12.1, 97, 410000011)
-
-
+  spawnMobile("dungeon2", "tps_battle_droid", 300, -16.3, -7.0, -33.0, 86, 410000021)
+  spawnMobile("dungeon2", "tps_battle_droid", 300, -27.4, -7.0, -33.0, 85, 410000021)
 end
