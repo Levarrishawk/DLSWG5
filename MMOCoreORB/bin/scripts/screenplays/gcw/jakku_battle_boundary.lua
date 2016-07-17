938119decd9c2ba5ffe4dc35335942f05744fda3
Levarrishawk <levarrishawk@gmail.com>
2016-07-16 23:43:06 -0400

    [iteration] jakku active area

--- MMOCoreORB/bin/scripts/screenplays/gcw/jakku_battle_boundary.lua
+++ MMOCoreORB/bin/scripts/screenplays/gcw/jakku_battle_boundary.lua
@@ -17,7 +17,7 @@ function jakkuBattleBoundaryScreenPlay:start()
 end
   
 function jakkuBattleBoundaryScreenPlay:spawnActiveAreas()
-  local pSpawnArea = spawnSceneObject("jakku", "object/active_area.iff", -5945, 20, 5774, 0, math.rad(0))
+  local pSpawnArea = spawnActiveArea("jakku", "object/active_area.iff", -5945, 20, 5774, 0, 0)
     
   if (pSpawnArea ~= nil) then
     local activeArea = LuaActiveArea(pSpawnArea)
@@ -41,31 +41,12 @@ function jakkuBattleBoundaryScreenPlay:notifySpawnArea(pActiveArea, pMovingObjec
     end
     
     if (player:isImperial() or player:isRebel()) then
-      createEvent(1, "jakku_battle", "handlePvpZone", pMovingObject)
       PlayerObject:sendSystemMessage("You have entered The Battle of Jakku.")
-    else
-      PlayerObject:sendSystemMessage("You have entered The Battle of Jakku.")
-   --   player:teleport(jakku, -5945, 20, 5774, 0)
     end
     return 0
   end)
 end
 
---Handles the setting of factional status
---[[
-function jakkuBattleBoundaryScreenPlay:handlePvpZone(pPlayer)
-  ObjectManager.withCreatureAndPlayerObject(pPlayer, function(player, playerObject)
-    deleteData(player:getObjectID() .. ":changingFactionStatus")
-    if (playerObject:isCovert() or playerObject:isOnLeave()) then
-      playerObject:setFactionStatus(2)
-    end
-  end)
-
-end
---]]
-
-
---Simply sends a system message
 function jakkuBattleBoundaryScreenPlay:notifySpawnAreaLeave(pActiveArea, pMovingObject)
   
   if (not SceneObject(pMovingObject):isCreatureObject()) then
