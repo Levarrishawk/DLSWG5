be163ea64f3443ec42d240810560ab38c3aa5b93
Levarrishawk <levarrishawk@gmail.com>
2016-07-17 06:56:46 -0400

    [iteration] neutral player removal.

--- MMOCoreORB/bin/scripts/screenplays/gcw/jakku_battle_boundary.lua
+++ MMOCoreORB/bin/scripts/screenplays/gcw/jakku_battle_boundary.lua
@@ -42,24 +42,17 @@ function pvp:notifySpawnArea(pActiveArea, pMovingObject)
     if (player:isImperial() or player:isRebel()) then     
       player:sendSystemMessage("You have entered the Battle of Jakku!")
     end
-    return 0
+    
+    
+    if (player:isNeutral()) then
+      player:sendSystemMessage("You must be a member of a faction to join the Battle of Jakku!")
+      player:teleport(tatooine, -5945, 20, 5774, 0)
+      end
+    return 0    
   end)
 end
 
---Handles the setting of factional status
---[[
-function pvp:handlePvpZone(pPlayer)
-  ObjectManager.withCreatureAndPlayerObject(pPlayer, function(player, playerObject)
-    deleteData(player:getObjectID() .. ":changingFactionStatus")
-    if (playerObject:isCovert() or playerObject:isOnLeave()) then
-      playerObject:setFactionStatus(2)
-    end
-  end)
 
-end
---]]
-
---Simply sends a system message
 function pvp:notifySpawnAreaLeave(pActiveArea, pMovingObject)
   
   if (not SceneObject(pMovingObject):isCreatureObject()) then
@@ -79,71 +72,4 @@ function pvp:notifySpawnAreaLeave(pActiveArea, pMovingObject)
   end)
 end
 
-
---[[local ObjectManager = require("managers.object.object_manager")
-
-jakkuBattleBoundaryScreenPlay = ScreenPlay:new {
-  numberOfActs = 1,
-    questString = "jakku_battle",
-    states = {onleave = 1, overt = 2},
-    questdata = Object:new {
-      activePlayerName = "initial",
-      }
-}
-  
-registerScreenPlay("jakkuBattleBoundaryScreenPlay", true)
-  
-function jakkuBattleBoundaryScreenPlay:start()
-      self:spawnActiveAreas()
-end
-  
-function jakkuBattleBoundaryScreenPlay:spawnActiveAreas()
-  local pActiveArea = spawnActiveArea("jakku", "object/active_area.iff", -5945, 20, 5774, 0, 0)
-    
-  if (pActiveArea ~= nil) then
-    local activeArea = LuaActiveArea(pActiveArea)
-          activeArea:setCellObjectID(0)
-          activeArea:setRadius(1000)
-          createObserver(ENTEREDAREA, "jakku_battle", "notifySpawnArea", pActiveArea)
-          createObserver(EXITEDAREA, "jakku_battle", "notifySpawnAreaLeave", pActiveArea)
-      end
-end
- 
---checks if player enters the zone, and what to do with them.
-function jakkuBattleBoundaryScreenPlay:notifySpawnArea(pActiveArea, pMovingObject)
-  
-  if (pMovingObject == nil or pActiveArea == nil or not SceneObject(pMovingObject):isCreatureObject()) then
-    return 0
-  end
-
-  if (CreatureObject(pMovingObject):isAiAgent() and not AiAgent(pMovingObject):isPet()) then
-    return 0
-  end
- 
-  return ObjectManager.withCreatureObject(pMovingObject, function(player)
-
-      CreatureObject(pPlayer):sendSystemMessage("You have entered The Battle of Jakku.")
-    
-  end)
-end
-
-function jakkuBattleBoundaryScreenPlay:notifySpawnAreaLeave(pActiveArea, pMovingObject)
-  
-  if (pMovingObject == nil or pActiveArea == nil or not SceneObject(pMovingObject):isCreatureObject()) then
-    return 0
-  end
-  
-  return ObjectManager.withCreatureObject(pMovingObject, function(player)
-    if (CreatureObject:isAiAgent()) then
-      return 0
-    end
-    
-    if (player:isImperial() or player:isRebel()) then
-      CreatureObject(pPlayer):sendSystemMessage("You are not authorized to leave the Battle Area!")
-      CreatureObject(pPlayer):teleport(jakku, -5945, 20, 5774, 0)
-    end
-    return 0
-  end)
-end
---]]
 
