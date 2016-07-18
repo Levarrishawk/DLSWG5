722bf164deacdc60fc87d4301692f18209ff05e0
TheSynge <john0684@hotmail.com>
2016-07-18 14:26:09 -0600

    added npc's to serverobjects

--- MMOCoreORB/bin/scripts/mobile/heroics/tps/serverobjects.lua
+++ MMOCoreORB/bin/scripts/mobile/heroics/tps/serverobjects.lua
@@ -2,4 +2,6 @@
 includeFile("heroics/tps/tps_battle_droid.lua")
 includeFile("heroics/tps/tps_black_sun_guard.lua")
 includeFile("heroics/tps/tps_black_sun_assassin.lua")
+includeFile("heroics/tps/tps_boss1.lua")
+includeFile("heroics/tps/tps_boss1_add.lua")
 
