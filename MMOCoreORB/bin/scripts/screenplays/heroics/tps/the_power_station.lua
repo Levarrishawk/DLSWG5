20b87d875c41c6b6a7c4587a2698d4fa435546e6
TheSynge <john0684@hotmail.com>
2016-07-18 15:01:46 -0600

    me, fucking around with scripts that wont work

--- MMOCoreORB/bin/scripts/screenplays/heroics/tps/the_power_station.lua
+++ MMOCoreORB/bin/scripts/screenplays/heroics/tps/the_power_station.lua
@@ -1,3 +1,5 @@
+local ObjectManager = require("managers.object.object_manager")
+
 ThePowerStationScreenPlay = ScreenPlay:new {
 	numberOfActs = 1,
 
@@ -20,7 +22,7 @@ function ThePowerStationScreenPlay:spawnMobiles()
   spawnMobile("dungeon2", "tps_battle_droid", 300, -21.9, -7.0, -4.8, 178, 410000012)
   spawnMobile("dungeon2", "tps_battle_droid", 300, -16.8, -7.0, 0.3, -85, 410000012)
   
-  --"power room (big blue light place)" area to the right leading to first boss
+  --"power room (big blue light place first floor)" area to the right leading to first boss
   spawnMobile("dungeon2", "tps_black_sun_guard", 300, -12.1, -7.0, 0.1, 91, 410000018)
   spawnMobile("dungeon2", "tps_black_sun_guard", 300, -5.5, -7.0, 0.1, -99, 410000018)  
   spawnMobile("dungeon2", "tps_black_sun_assassin", 300, 0.1, -7.0, -5.8, -70, 410000018)  
@@ -30,4 +32,18 @@ function ThePowerStationScreenPlay:spawnMobiles()
   spawnMobile("dungeon2", "tps_black_sun_assassin", 900, 20.8, -7.0, 5.2, -171, 410000020)
   spawnMobile("dungeon2", "tps_black_sun_assassin", 900, 23.2, -7.0, 5.2, -171, 410000020)
   
+  local boss = LuaCreatureObject(pBoss)
 end
+
+
+function the_power_station:bossfight1(pBoss)
+
+  local player = LuaCreatureObject(player)
+  
+  if (player:getParentID() == 410000018) then
+    spawnMobile("dungeon2", "tps_boss1", 1800, 15.0, -7.0, 16.2, 92, 410000016)
+   end
+      
+ end
+    
+
