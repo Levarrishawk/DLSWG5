4de3d7c16332b6f9254fbef0b32bd34b2a1a8f6f
Levarrishawk <levarrishawk@gmail.com>
2016-07-18 00:20:15 -0400

    [adjusted] structure position

--- MMOCoreORB/bin/scripts/screenplays/gcw/jakku_battle_boundary.lua
+++ MMOCoreORB/bin/scripts/screenplays/gcw/jakku_battle_boundary.lua
@@ -19,7 +19,7 @@ end
 
 function pvp:spawnSceneObjects()
   spawnSceneObject("jakku", "object/battlefield_marker/battlefield_marker_512m.iff", -5945, 20, 5774, 0, math.rad(0) )
-  spawnSceneObject("jakku", "object/static/structure/general/landing_pad_shuttle.iff", 4325, 7,-5097, 0, math.rad(0) )
+  spawnSceneObject("jakku", "object/static/structure/general/landing_pad_shuttle.iff", 4325, 7.8, -5097, 0, math.rad(0) )
 end
 
 function pvp:spawnMobiles()
