d19706ddd4468c23770f95f09011c5c45189cb5c
Levarrishawk <levarrishawk@gmail.com>
2016-07-20 21:27:02 -0400

    [iteration] error crushing

--- MMOCoreORB/src/server/zone/objects/creature/commands/PvpCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/PvpCommand.h
@@ -25,10 +25,10 @@ public:
 
 		PlayerObject* ghost = creature->getPlayerObject();
 
-		if (ghost->getFactionStatus() == ONLEAVE && (ghost->isRebel() || ghost->isImperial()) )
-			ghost->setFactionStatus(OVERT);  // Check to see if player is rebel or imperial and onleave then make them overt.
-		else if (ghost->getFactionStatus() == OVERT && (ghost->isRebel() || ghost->isImperial()))
-			ghost->setFactionStatus(ONLEAVE); // check to see if player is rebel or imperial and overt and make them on leave.
+		if (ghost->getFactionStatus() == 0 && (ghost->isRebel() || ghost->isImperial()) )
+			ghost->setFactionStatus(2);  // Check to see if player is rebel or imperial and onleave then make them overt.
+		else if (ghost->getFactionStatus() == 2 && (ghost->isRebel() || ghost->isImperial()))
+			ghost->setFactionStatus(0); // check to see if player is rebel or imperial and overt and make them on leave.
 		else
 			ghost->sendSystemMessage("You must be a member of a faction to use this command.");
 
