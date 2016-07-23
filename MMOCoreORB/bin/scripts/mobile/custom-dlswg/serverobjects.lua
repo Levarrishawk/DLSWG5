6852ad84af596defd8eb55698ba8bb3157690604
Levarrishawk <levarrishawk@gmail.com>
2016-07-22 22:54:11 -0400

    [iteration] cratertown

--- MMOCoreORB/bin/scripts/mobile/custom-dlswg/serverobjects.lua
+++ MMOCoreORB/bin/scripts/mobile/custom-dlswg/serverobjects.lua
@@ -1,2 +1,3 @@
 includeFile("custom-dlswg/reb_chasm_dungeon_teleporter.lua")
 includeFile("custom-dlswg/punishment_acklay.lua")
+includeFile("niimo_the_hutt.lua")
--- MMOCoreORB/src/server/zone/objects/creature/commands/PvpCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/PvpCommand.h
@@ -1,12 +1,13 @@
 /*
- *
- * Broken!
 #ifndef PVPCOMMAND_H_
 #define PVPCOMMAND_H_
 
-#include "server/zone/objects/scene/SceneObject.h"
+
 #include "server/zone/objects/creature/CreatureObject.h"
+#include "server/zone/objects/scene/SceneObject.h"
+#include "server/zone/objects/tangible/TangibleObject.h"
 #include "server/zone/objects/player/PlayerObject.h"
+#include "server/zone/objects/player/FactionStatus.h"
 #include "server/zone/managers/player/PlayerManager.h"
 
 class PvpCommand : public QueueCommand {
@@ -25,11 +26,28 @@ public:
 		if (!checkInvalidLocomotions(creature))
 			return INVALIDLOCOMOTION;
 
+		CreatureObject* player = cast<CreatureObject*>(creature);
+
+		ManagedReference<SceneObject*> obj = server->getZoneServer()->getObject(target);
+
 		ManagedReference<PlayerObject*> ghost = creature->getPlayerObject();
 
-		if (ghost->getFactionStatus() == 0 && (ghost->getFaction()== "rebel" || ghost->getFaction()== "imperial"))
+		TangibleObject* tano = cast<TangibleObject*>( obj.get());
+
+		uint32 pvpStatus = tano->getPvpStatusBitmask();
+		uint32 optionsBitmask = tano->getOptionsBitmask();
+		uint32 intFaction = tano->getFaction();
+
+		String faction;
+		tokenizer.getStringToken(faction);
+
+		if (faction == "imperial" || faction == "rebel" || faction == "hutt") {
+					tano->setFaction(faction.hashCode());
+				}
+
+		if (ghost->getFactionStatus() == 0 && (faction = "rebel" || faction = "imperial"))
 			ghost->setFactionStatus(2);  // Check to see if player is rebel or imperial and onleave then make them overt.
-		else if (ghost->getFactionStatus() == 2 && (ghost->getFaction()== "rebel" || ghost->getFaction()== "imperial"))
+		else if (ghost->getFactionStatus() == 2 && (faction = "rebel" || faction = "imperial"))
 			ghost->setFactionStatus(0); // check to see if player is rebel or imperial and overt and make them on leave.
 		else
 			creature->sendSystemMessage("You must be a member of a faction to use this command.");
@@ -41,4 +59,5 @@ public:
 };
 
 #endif
+
 */
