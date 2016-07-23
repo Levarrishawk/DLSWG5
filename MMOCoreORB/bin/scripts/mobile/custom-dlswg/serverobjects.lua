baa308e0da349d1a47435824a915e0ed16e3cab8
Levarrishawk <levarrishawk@gmail.com>
2016-07-22 23:03:29 -0400

    [iteration] dumb rookie mistakes...

--- MMOCoreORB/bin/scripts/mobile/custom-dlswg/serverobjects.lua
+++ MMOCoreORB/bin/scripts/mobile/custom-dlswg/serverobjects.lua
@@ -1,3 +1,3 @@
 includeFile("custom-dlswg/reb_chasm_dungeon_teleporter.lua")
 includeFile("custom-dlswg/punishment_acklay.lua")
-includeFile("niima_the_hutt.lua")
+includeFile("custom-dlswg/niima_the_hutt.lua")
