052e4851bcd93a2d002515a3c6f02ad8d3853342
TheSynge <john0684@hotmail.com>
2016-07-31 17:27:30 -0600

    Vehicle Disable

--- MMOCoreORB/src/server/zone/objects/intangible/VehicleControlDeviceImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/intangible/VehicleControlDeviceImplementation.cpp
@@ -26,7 +26,7 @@ void VehicleControlDeviceImplementation::generateObject(CreatureObject* player) 
 	if (!isASubChildOf(player))
 		return;
 
-	if (player->getParent() != NULL || player->isInCombat()) {
+	if (player->getParent() != NULL || player->isInCombat() || player->getZone == "Jakku"()) {
 		player->sendSystemMessage("@pet/pet_menu:cant_call_vehicle"); // You can only unpack vehicles while Outside and not in Combat.
 		return;
 	}
