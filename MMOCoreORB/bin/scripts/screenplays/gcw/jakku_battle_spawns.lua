ccf134dfbe03d87fd07a0d8198084721a4bc1fd9
Levarrishawk <levarrishawk@gmail.com>
2016-07-31 22:27:12 -0400

    [iteration] jakku battle spawns (republic side)

--- MMOCoreORB/bin/scripts/screenplays/gcw/jakku_battle_spawns.lua
+++ MMOCoreORB/bin/scripts/screenplays/gcw/jakku_battle_spawns.lua
@@ -19,20 +19,39 @@ function JakkuBattleScreenPlay:spawnMobiles()
   
 --  START:  PvP Zone
   -- Republic Encampment (exterior)
-  spawnMobile("jakku", "at_xt", 60, -5764, 56, 5606, -40, 0)  
-  spawnMobile("jakku", "at_xt", 60, -5755, 52, 5552, -60, 0)
-  spawnMobile("jakku", "at_xt", 60, -5790, 56, 5535, -40, 0)
-  spawnMobile("jakku", "at_xt", 60, -5750, 50, 5531, -60, 0)
-  spawnMobile("jakku", "at_xt", 60, -5709, 46, 5531, -43, 0)
-  spawnMobile("jakku", "at_xt", 60, -5725, 47, 5508, -54, 0)
+  local pMobile = spawnMobile("jakku", "at_xt", 60, -5763, 45, 5505, -116, 0)
+        self:setMoodString(pMobile, "neutral")
+        AiAgent(pMobile):setAiTemplate("idlewander")
+        
+  pMobile = spawnMobile("jakku", "at_xt", 60, -5764, 56, 5606, -40, 0)
+  self:setMoodString(pMobile, "neutral")
+  AiAgent(pMobile):setAiTemplate("idlewander")
+  
+  pMobile = spawnMobile("jakku", "at_xt", 60, -5755, 52, 5552, -60, 0)
+  self:setMoodString(pMobile, "neutral")
+  AiAgent(pMobile):setAiTemplate("idlewander")
+  
+  pMobile = spawnMobile("jakku", "at_xt", 60, -5790, 56, 5535, -40, 0)
+  self:setMoodString(pMobile, "neutral")
+  AiAgent(pMobile):setAiTemplate("idlewander")
+  
+  pMobile = spawnMobile("jakku", "at_xt", 60, -5750, 50, 5531, -60, 0)
+  self:setMoodString(pMobile, "neutral")
+  AiAgent(pMobile):setAiTemplate("idlewander")
+  
+  pMobile = spawnMobile("jakku", "at_xt", 60, -5709, 46, 5531, -43, 0)
+  self:setMoodString(pMobile, "neutral")
+  AiAgent(pMobile):setAiTemplate("idlewander")
+  
+  pMobile = spawnMobile("jakku", "at_xt", 60, -5725, 47, 5508, -54, 0)
+  self:setMoodString(pMobile, "neutral")
+  AiAgent(pMobile):setAiTemplate("idlewander")
   
   spawnMobile("jakku", "fbase_rebel_heavy_trooper_extreme", 60, -5709.5, 48, 5503, -42, 0)
   spawnMobile("jakku", "fbase_rebel_heavy_trooper_extreme", 60, -5705, 48, 5507, -42, 0)
   spawnMobile("jakku", "fbase_rebel_commando_hard", 60, -5783, 58, 5549, -64, 0)
  
-  local pMobile = spawnMobile("jakku", "fbase_rebel_colonel_hard", 60, -5763, 45, 5505, -116, 0)
-        self:setMoodString(pMobile, "neutral")
-        AiAgent(pMobile):setAiTemplate("idlewander")
+  
   
 
 end
