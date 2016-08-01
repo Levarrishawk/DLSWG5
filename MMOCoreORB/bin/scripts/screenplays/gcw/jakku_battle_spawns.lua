00a98170a446cb4795629b9dd257542f965cf1a5
Levarrishawk <levarrishawk@gmail.com>
2016-07-31 21:51:46 -0400

    [iteration] Jakku republic battle spawns phase1

--- MMOCoreORB/bin/scripts/screenplays/gcw/jakku_battle_spawns.lua
+++ MMOCoreORB/bin/scripts/screenplays/gcw/jakku_battle_spawns.lua
@@ -16,7 +16,20 @@ end
 function JakkuBattleScreenPlay:spawnMobiles()
 --gladiator arena
   spawnMobile("jakku", "punishment_acklay", 1, 4454, 7.0, -5171, -84, 0)
-
+  
+--  START:  PvP Zone
+  -- Republic Encampment (exterior)
+  spawnMobile("jakku", "at_xt", 60, -5764, 56, 5606, -40, 0)  
+  spawnMobile("jakku", "at_xt", 60, -5755, 52, 5552, -60, 0)
+  spawnMobile("jakku", "at_xt", 60, -5790, 56, 5535, -40, 0)
+  spawnMobile("jakku", "at_xt", 60, -5750, 50, 5531, -60, 0)
+  spawnMobile("jakku", "at_xt", 60, -5709, 46, 5531, -43, 0)
+  spawnMobile("jakku", "at_xt", 60, -5725, 47, 5508, -54, 0)
+  
+  spawnMobile("jakku", "fbase_rebel_heavy_trooper_extreme", 60, -5709.5, 48, 5503, -42, 0)
+  spawnMobile("jakku", "fbase_rebel_heavy_trooper_extreme", 60, -5705, 48, 5507, -42, 0)
+  spawnMobile("jakku", "fbase_rebel_commando_hard", 60, -5783, 58, 5549, -64, 0)
+  spawnMobile("jakku", "fbase_rebel_colonel_hard", 60, -5780, 58, 5545, 116, 0)
   
 
 end
