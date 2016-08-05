62a70d522e9cf399b5a697e3ec57f7b6563a6acc
Levarrishawk <levarrishawk@gmail.com>
2016-08-05 03:13:14 -0400

    [iteration] Jakku Weapons Facility

--- MMOCoreORB/bin/scripts/mobile/dungeon/death_watch_bunker/death_watch_battle_droid.lua
+++ MMOCoreORB/bin/scripts/mobile/dungeon/death_watch_bunker/death_watch_battle_droid.lua
@@ -1,46 +1,46 @@
-tps_battle_droid = Creature:new {
-	objectName = "@mob/creature_names:mand_bunker_battle_droid",
-	socialGroup = "death_watch",
-	faction = "",
-	level = 145,
-	chanceHit = 5.5,
-	damageMin = 995,
-	damageMax = 1600,
-	baseXp = 12612,
-	baseHAM = 67000,
-	baseHAMmax = 88000,
-	armor = 2,
-	resists = {75,75,100,60,100,25,40,85,-1},
-	meatType = "",
-	meatAmount = 0,
-	hideType = "",
-	hideAmount = 0,
-	boneType = "",
-	boneAmount = 0,
-	milk = 0,
-	tamingChance = 0,
-	ferocity = 0,
-	pvpBitmask = AGGRESSIVE + ATTACKABLE + ENEMY,
-	creatureBitmask = KILLER,
-	optionsBitmask = AIENABLED,
-	diet = HERBIVORE,
-	scale = 1.15,
+death_watch_battle_droid = Creature:new {
+  objectName = "@mob/creature_names:mand_bunker_battle_droid",
+  socialGroup = "death_watch",
+  faction = "",
+  level = 134,
+  chanceHit = 5.5,
+  damageMin = 795,
+  damageMax = 1300,
+  baseXp = 12612,
+  baseHAM = 56000,
+  baseHAMmax = 68000,
+  armor = 2,
+  resists = {75,75,100,60,100,25,40,85,-1},
+  meatType = "",
+  meatAmount = 0,
+  hideType = "",
+  hideAmount = 0,
+  boneType = "",
+  boneAmount = 0,
+  milk = 0,
+  tamingChance = 0,
+  ferocity = 0,
+  pvpBitmask = AGGRESSIVE + ATTACKABLE + ENEMY,
+  creatureBitmask = KILLER,
+  optionsBitmask = AIENABLED,
+  diet = HERBIVORE,
+  scale = 1.15,
 
-	templates = {
-		"object/mobile/death_watch_battle_droid.iff",
-		"object/mobile/death_watch_battle_droid_02.iff",
-		"object/mobile/death_watch_battle_droid_03.iff"},
-	lootGroups = {
-		{
-			groups = {
-				{}
-			},
-			lootChance = 500000
-		}
-	},
-	weapons = {"battle_droid_weapons"},
-	conversationTemplate = "",
-	attacks = merge(pistoleermaster,carbineermaster,marksmanmaster)
+  templates = {
+    "object/mobile/death_watch_battle_droid.iff",
+    "object/mobile/death_watch_battle_droid_02.iff",
+    "object/mobile/death_watch_battle_droid_03.iff"},
+  lootGroups = {
+    {
+      groups = {
+        {group = "death_watch_bunker_commoners", chance = 10000000}
+      },
+      lootChance = 500000
+    }
+  },
+  weapons = {"battle_droid_weapons"},
+  conversationTemplate = "",
+  attacks = merge(pistoleermaster,carbineermaster,marksmanmaster)
 }
 
-CreatureTemplates:addCreatureTemplate(tps_battle_droid, "tps_battle_droid")
+CreatureTemplates:addCreatureTemplate(death_watch_battle_droid, "death_watch_battle_droid")
---
+++ MMOCoreORB/bin/scripts/mobile/dungeon/jakku_weapons_facility/serverobjects.lua
@@ -0,0 +1,2 @@
+includeFile("dungeon/jakku_weapons_facility/jakku_battle_droid.lua")
+
