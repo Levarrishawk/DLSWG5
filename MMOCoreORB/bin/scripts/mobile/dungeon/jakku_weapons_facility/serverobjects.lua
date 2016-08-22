7f05898c9283c9186e5cf26d655fd9a78b8421eb
Levarrishawk <levarrishawk@gmail.com>
2016-08-21 20:04:49 -0400

    [fixed] wrong path reference

--- MMOCoreORB/bin/scripts/mobile/dungeon/jakku_weapons_facility/serverobjects.lua
+++ MMOCoreORB/bin/scripts/mobile/dungeon/jakku_weapons_facility/serverobjects.lua
@@ -1,3 +1,3 @@
 includeFile("dungeon/jakku_weapons_facility/jakku_battle_droid.lua")
 includeFile("dungeon/jakku_weapons_facility/jakku_fast_battle_droid.lua")
-includeFile("dungeon/jakku_black_trooper_1.lua")
+includeFile("dungeon/jakku_weapons_facility/jakku_black_trooper_1.lua")
