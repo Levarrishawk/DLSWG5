38b2a1e3aced8a5cf3df3effc924806be3c55efa
Levarrishawk <levarrishawk@gmail.com>
2016-08-25 02:40:20 -0400

    [iteration] Faction Bosses

--- MMOCoreORB/bin/scripts/mobile/faction/rebel/jakku_rebel_general.lua
+++ MMOCoreORB/bin/scripts/mobile/faction/rebel/jakku_rebel_general.lua
@@ -27,7 +27,7 @@ jakku_rebel_general = Creature:new {
 	creatureBitmask = PACK + KILLER,
 	optionsBitmask = AIENABLED + CONVERSABLE,
 	diet = HERBIVORE,
-	scale = 2,
+	scale = 1.5,
 
 	templates = {
 		"object/mobile/dressed_rebel_high_general_human_female_01.iff",
