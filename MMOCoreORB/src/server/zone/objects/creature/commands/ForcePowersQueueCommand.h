3242790861370e8e89030e47525253ff651b1ffb
Levarrishawk <levarrishawk@gmail.com>
2017-01-01 00:32:03 -0500

    [experiment] iteration 9

--- MMOCoreORB/src/server/zone/objects/creature/commands/ForcePowersQueueCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/ForcePowersQueueCommand.h
@@ -74,7 +74,7 @@ public:
 				}
 
 				if (playerObject != NULL)
-					playerObject->setForcePower(creature->getHAM(CreatureAttribute::ACTION) - forceCost);
+					playerObject->inflictDamage(playerObject, CreatureAttribute::ACTION, action, true, true, true);//setForcePower(creature->getHAM(CreatureAttribute::ACTION) - forceCost);
 
 			} catch (Exception& e) {
 				error("unreported exception caught in ForcePowersQueueCommand::doCombatAction");
