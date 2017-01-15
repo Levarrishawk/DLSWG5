61dc2e5dc91fb386936e036b5c8d6d96ee9d307c
Levarrishawk <levarrishawk@gmail.com>
2017-01-14 21:56:48 -0500

    [iteration] Visibility

--- MMOCoreORB/src/server/zone/managers/visibility/VisibilityManager.cpp
+++ MMOCoreORB/src/server/zone/managers/visibility/VisibilityManager.cpp
@@ -125,7 +125,7 @@ void VisibilityManager::login(CreatureObject* creature) {
 	if (ghost != NULL) {
 
 		//You only gain visibility after completing the padawan trials
-		if(!creature->hasSkill(skill->getSkillName().indexOf("combat_"))) {
+		if(!creature->hasSkill(getSkillName().beginsWith("combat_"))) {
 			//info("Player " + creature->getFirstName() + " does not qualify for visibility", true);
 			return;
 		}
