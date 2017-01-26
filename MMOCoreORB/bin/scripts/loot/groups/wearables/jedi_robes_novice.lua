cc990ed2483f013cb9292b82d6bce4d10f74b14c
Levarrishawk <levarrishawk@gmail.com>
2017-01-25 22:51:24 -0500

    [added] Tier 1 Jedi Robe Loot Group

---
+++ MMOCoreORB/bin/scripts/loot/groups/wearables/jedi_robes_novice.lua
@@ -0,0 +1,13 @@
+jedi_robes_novice = {
+	description = "",
+	minimumLevel = 0,
+	maximumLevel = 0,
+	lootItems = {
+		-- Common
+		
+		{itemTemplate = "robe_jedi_dark_s01", weight = 2000000},
+    {itemTemplate = "robe_jedi_light_s01", weight = 2000000},
+	}
+}
+
+addLootGroupTemplate("jedi_robes_novice", jedi_robes_novice)
