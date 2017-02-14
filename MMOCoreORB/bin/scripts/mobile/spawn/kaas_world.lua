9ab41ba2d879173911e0178e73504748712c0532
Levarrishawk <levarrishawk@gmail.com>
2017-02-14 04:53:05 -0500

    [iteration]

--- MMOCoreORB/bin/scripts/mobile/spawn/kaas_world.lua
+++ MMOCoreORB/bin/scripts/mobile/spawn/kaas_world.lua
@@ -1,170 +1,7 @@
 kaas_world = {
 
-	lairSpawns = 
-	{
-    		{
-          lairTemplateName = "kaas_kell_dragon_pack",
-          spawnLimit = -1,
-          minDifficulty = 100,
-          maxDifficulty = 110,
-          numberToSpawn = 0,
-          weighting = 15,
-          size = 40
-        },
-    		{
-    			lairTemplateName = "kaas_ysalamiri_pack",
-    			spawnLimit = -1,
-    			minDifficulty = 100,
-    			maxDifficulty = 110,
-    			numberToSpawn = 0,
-    			weighting = 15,
-    			size = 40
-    		},
-    		{
-    			lairTemplateName = "kaas_vine_cat_lair",
-    			spawnLimit = -1,
-    			minDifficulty = 100,
-    			maxDifficulty = 110,
-    			numberToSpawn = 0,
-    			weighting = 15,
-    			size = 40
-    		},
-    		{
-    			lairTemplateName = "kaas_sleen_lair",
-    			spawnLimit = -1,
-    			minDifficulty = 100,
-    			maxDifficulty = 110,
-    			numberToSpawn = 0,
-    			weighting = 15,
-    			size = 40
-    		},
-    		{
-    			lairTemplateName = "kaas_mailoc_swarm",
-    			spawnLimit = -1,
-    			minDifficulty = 100,
-    			maxDifficulty = 110,
-    			numberToSpawn = 0,
-    			weighting = 15,
-    			size = 40
-    		},
-    		{
-    			lairTemplateName = "kaas_gundark_pack",
-    			spawnLimit = -1,
-    			minDifficulty = 100,
-    			maxDifficulty = 130,
-    			numberToSpawn = 0,
-    			weighting = 15,
-    			size = 40
-    		},
-    		{
-    			lairTemplateName = "dathomir_reptilian_flyer_flock_neutral_none",
-    			spawnLimit = -1,
-    			minDifficulty = 10,
-    			maxDifficulty = 19,
-    			numberToSpawn = 0,
-    			weighting = 15,
-    			size = 40
-    		},
-    		{
-    			lairTemplateName = "dathomir_reptilian_ancient_flyer_flock_neutral_none",
-    			spawnLimit = -1,
-    			minDifficulty = 16,
-    			maxDifficulty = 25,
-    			numberToSpawn = 0,
-    			weighting = 15,
-    			size = 40
-    		},
-    		{
-    			lairTemplateName = "dathomir_reptilian_mature_flyer_lair_neutral_large",
-    			spawnLimit = -1,
-    			minDifficulty = 20,
-    			maxDifficulty = 24,
-    			numberToSpawn = 0,
-    			weighting = 15,
-    			size = 40
-    		},
-    		{
-    			lairTemplateName = "dantooine_dark_side_savage_neutral_none",
-    			spawnLimit = -1,
-    			minDifficulty = 75,
-    			maxDifficulty = 79,
-    			numberToSpawn = 0,
-    			weighting = 15,
-    			size = 40
-    		},
-    		{
-    			lairTemplateName = "dantooine_force_renegade_neutral_none",
-    			spawnLimit = -1,
-    			minDifficulty = 80,
-    			maxDifficulty = 84,
-    			numberToSpawn = 0,
-    			weighting = 15,
-    			size = 40
-    		},
-    		{
-    			lairTemplateName = "dantooine_force_archaist_neutral_none",
-    			spawnLimit = -1,
-    			minDifficulty = 90,
-    			maxDifficulty = 94,
-    			numberToSpawn = 0,
-    			weighting = 15,
-    			size = 40
-    		},
-    		{
-    			lairTemplateName = "dantooine_force_crypt_crawler_neutral_none",
-    			spawnLimit = -1,
-    			minDifficulty = 95,
-    			maxDifficulty = 99,
-    			numberToSpawn = 0,
-    			weighting = 15,
-    			size = 40
-    		},
-    		{
-    			lairTemplateName = "dantooine_force_wilder_neutral_none",
-    			spawnLimit = -1,
-    			minDifficulty = 115,
-    			maxDifficulty = 119,
-    			numberToSpawn = 0,
-    			weighting = 15,
-    			size = 40
-    		},
-    		{
-    			lairTemplateName = "dantooine_force_crystal_hunter_neutral_none",
-    			spawnLimit = -1,
-    			minDifficulty = 115,
-    			maxDifficulty = 119,
-    			numberToSpawn = 0,
-    			weighting = 15,
-    			size = 40
-    		},
-    		{
-          lairTemplateName = "dathomir_kwi_pack_neutral_none",
-          spawnLimit = -1,
-          minDifficulty = 8,
-          maxDifficulty = 12,
-          numberToSpawn = 0,
-          weighting = 15,
-          size = 20
-        },
-        {
-          lairTemplateName = "dathomir_shear_mite_pack_neutral_none",
-          spawnLimit = -1,
-          minDifficulty = 8,
-          maxDifficulty = 17,
-          numberToSpawn = 0,
-          weighting = 15,
-          size = 25
-        },
-        {
-          lairTemplateName = "dathomir_kwi_lair_neutral_small",
-          spawnLimit = -1,
-          minDifficulty = 10,
-          maxDifficulty = 14,
-          numberToSpawn = 0,
-          weighting = 15,
-          size = 20
-        },
-        {
+  lairSpawns = {
+    {
       lairTemplateName = "dathomir_verne_herd_neutral_none",
       spawnLimit = -1,
       minDifficulty = 6,
@@ -831,6 +668,24 @@ kaas_world = {
       size = 25
     },
     {
+      lairTemplateName = "dathomir_mtn_clan_initiate_squad_neutral_none",
+      spawnLimit = -1,
+      minDifficulty = 50,
+      maxDifficulty = 54,
+      numberToSpawn = 0,
+      weighting = 15,
+      size = 25
+    },
+    {
+      lairTemplateName = "dathomir_mtn_clan_initiate_neutral_large_theater",
+      spawnLimit = -1,
+      minDifficulty = 50,
+      maxDifficulty = 54,
+      numberToSpawn = 0,
+      weighting = 15,
+      size = 30
+    },
+    {
       lairTemplateName = "dathomir_rancor_pack_neutral_none",
       spawnLimit = -1,
       minDifficulty = 50,
@@ -876,12 +731,84 @@ kaas_world = {
       size = 25
     },
     {
+      lairTemplateName = "dathomir_mtn_clan_enclave_neutral_large_theater",
+      spawnLimit = -1,
+      minDifficulty = 50,
+      maxDifficulty = 159,
+      numberToSpawn = 0,
+      weighting = 8,
+      size = 30
+    },
+    {
+      lairTemplateName = "dathomir_mtn_clan_rancor_tamer_squad_neutral_none",
+      spawnLimit = -1,
+      minDifficulty = 55,
+      maxDifficulty = 79,
+      numberToSpawn = 0,
+      weighting = 15,
+      size = 30
+    },
+    {
+      lairTemplateName = "dathomir_mtn_clan_rancor_tamer_neutral_large_theater",
+      spawnLimit = -1,
+      minDifficulty = 55,
+      maxDifficulty = 79,
+      numberToSpawn = 0,
+      weighting = 15,
+      size = 30
+    },
+    {
       lairTemplateName = "dathomir_malkloc_plainswalker_neutral_none",
       spawnLimit = -1,
       minDifficulty = 55,
       maxDifficulty = 84,
       numberToSpawn = 0,
       weighting = 15,
+      size = 30
+    },
+    {
+      lairTemplateName = "dathomir_nightsister_rancor_tamer_neutral_none",
+      spawnLimit = -1,
+      minDifficulty = 55,
+      maxDifficulty = 89,
+      numberToSpawn = 0,
+      weighting = 15,
+      size = 30
+    },
+    {
+      lairTemplateName = "dathomir_nightsister_spell_weaver_escort_neutral_none",
+      spawnLimit = -1,
+      minDifficulty = 60,
+      maxDifficulty = 109,
+      numberToSpawn = 0,
+      weighting = 8,
+      size = 25
+    },
+    {
+      lairTemplateName = "dathomir_nightsister_spell_weaver_camp_neutral_large_theater",
+      spawnLimit = -1,
+      minDifficulty = 60,
+      maxDifficulty = 109,
+      numberToSpawn = 0,
+      weighting = 8,
+      size = 30
+    },
+    {
+      lairTemplateName = "dathomir_nightsister_elder_escort_neutral_none",
+      spawnLimit = -1,
+      minDifficulty = 60,
+      maxDifficulty = 279,
+      numberToSpawn = 0,
+      weighting = 5,
+      size = 25
+    },
+    {
+      lairTemplateName = "dathomir_nightsister_elder_enclave_neutral_large_theater",
+      spawnLimit = -1,
+      minDifficulty = 60,
+      maxDifficulty = 279,
+      numberToSpawn = 0,
+      weighting = 5,
       size = 30
     },
     {
@@ -892,7 +819,25 @@ kaas_world = {
       numberToSpawn = 0,
       weighting = 8,
       size = 30
-    },  
+    },
+    {
+      lairTemplateName = "dathomir_mtn_clan_outcast_camp_neutral_small_theater",
+      spawnLimit = -1,
+      minDifficulty = 75,
+      maxDifficulty = 79,
+      numberToSpawn = 0,
+      weighting = 15,
+      size = 20
+    },
+    {
+      lairTemplateName = "dathomir_mtn_clan_huntress_squad_neutral_none",
+      spawnLimit = -1,
+      minDifficulty = 75,
+      maxDifficulty = 79,
+      numberToSpawn = 0,
+      weighting = 15,
+      size = 25
+    },
     {
       lairTemplateName = "dathomir_rancor_mutant_pack_neutral_none",
       spawnLimit = -1,
@@ -901,6 +846,33 @@ kaas_world = {
       numberToSpawn = 0,
       weighting = 8,
       size = 30
+    },
+    {
+      lairTemplateName = "dathomir_mtn_clan_dragoon_squad_neutral_none",
+      spawnLimit = -1,
+      minDifficulty = 75,
+      maxDifficulty = 94,
+      numberToSpawn = 0,
+      weighting = 15,
+      size = 25
+    },
+    {
+      lairTemplateName = "dathomir_mtn_clan_arch_witch_squad_neutral_none",
+      spawnLimit = -1,
+      minDifficulty = 75,
+      maxDifficulty = 109,
+      numberToSpawn = 0,
+      weighting = 8,
+      size = 25
+    },
+    {
+      lairTemplateName = "dathomir_mtn_clan_councilwoman_escort_neutral_none",
+      spawnLimit = -1,
+      minDifficulty = 75,
+      maxDifficulty = 254,
+      numberToSpawn = 0,
+      weighting = 5,
+      size = 25
     },
     {
       lairTemplateName = "dathomir_rancor_enraged_pack_neutral_none",
@@ -919,7 +891,25 @@ kaas_world = {
       numberToSpawn = 0,
       weighting = 8,
       size = 30
-    },  
+    },
+    {
+      lairTemplateName = "dathomir_nightsister_rangers_neutral_none",
+      spawnLimit = -1,
+      minDifficulty = 81,
+      maxDifficulty = 85,
+      numberToSpawn = 0,
+      weighting = 15,
+      size = 25
+    },
+    {
+      lairTemplateName = "dathomir_nightsister_outcast_camp_neutral_small_theater",
+      spawnLimit = -1,
+      minDifficulty = 81,
+      maxDifficulty = 85,
+      numberToSpawn = 0,
+      weighting = 15,
+      size = 20
+    },
     {
       lairTemplateName = "dathomir_rancor_enraged_bull_lair_neutral_large",
       spawnLimit = -1,
@@ -938,8 +928,43 @@ kaas_world = {
       weighting = 6,
       size = 30
     },
-    
-	}
+    {
+      lairTemplateName = "dathomir_nightsister_stalker_neutral_none",
+      spawnLimit = -1,
+      minDifficulty = 96,
+      maxDifficulty = 100,
+      numberToSpawn = 0,
+      weighting = 15,
+      size = 25
+    },
+    {
+      lairTemplateName = "dathomir_rancor_ancient_bull_pack_neutral_none",
+      spawnLimit = -1,
+      minDifficulty = 98,
+      maxDifficulty = 102,
+      numberToSpawn = 0,
+      weighting = 6,
+      size = 30
+    },
+    {
+      lairTemplateName = "dathomir_rancor_ancient_bull_lair_neutral_large",
+      spawnLimit = -1,
+      minDifficulty = 98,
+      maxDifficulty = 102,
+      numberToSpawn = 0,
+      weighting = 6,
+      size = 30
+    },
+    {
+      lairTemplateName = "global_black_sun_neutral_none",
+      spawnLimit = -1,
+      minDifficulty = 76,
+      maxDifficulty = 107,
+      numberToSpawn = 0,
+      weighting = 3,
+      size = 25
+    },
+  }
 }
 
 addSpawnGroup("kaas_world", kaas_world);
